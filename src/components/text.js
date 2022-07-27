d1b8c37accd6ad6f6511532354c62eb0261848b8
necmigunduz <necmigunduz@gmail.com>
2022-07-28 00:18:21 +0300

    Add @media screen

--- src/components/text.js
+++ src/components/text.js
@@ -3,7 +3,8 @@ import React from 'react'
 const Text = () => {
   return (
     <div className='right mb-5'>
-        <h3 className="text-header font-weight-bold h2 w-75 mb-3">Everyday items, we have something to suit every occasion.</h3>
+        <h3 className="text-header full-text font-weight-bold h2 w-75 mb-3">Everyday items, we have something to suit every occasion.</h3>
+        <h3 className="mobile-text">Everyday tops, we have something to suit every occasion.</h3>
         <p className="text-body w-75">At suspendisse augue lectus arcu, accumsan ut sit posuere vitae sit tincidut semper eu proin leo gravida cursus.</p>
         <h5 className="products-header">Shop All Everyday Items</h5>
     </div>
