a886ae5255e69ae574cf0ea1c912fb5e29093dcb
necmigunduz <necmigunduz@gmail.com>
2022-07-27 18:41:11 +0300

    Style dots

---
+++ src/components/circle.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Circle = () => {
+  return (
+    <div
+      className="circle"
+      style={{
+        width: "11px",
+        height: "11px",
+        border: "1px solid",
+        borderRadius: "50px",
+        backgroundColor: "gray"
+      }}
+    ></div>
+  );
+};
+
+export default Circle;
--- src/components/product.js
+++ src/components/product.js
@@ -1,13 +1,16 @@
 import React from "react";
-import dots from '../assets/dots.png'
+import Circle from "./circle";
 
-const Product = ({source, price, brand}) => {
+const Product = ({ source, price, brand }) => {
   return (
     <div className="product">
-      <img src={source} alt={source} width={'90%'} />
-      <p className="brand-line">{brand}</p>
+      <img src={source} alt={source} width={"90%"} />
+      <p className="brand-line mt-2">{brand}</p>
       <div className="bottom d-flex justify-content-between">
-        <p className="price">{price}</p>
+        <p className="price-line">{price}</p>
+        <div className="circles d-flex p-1">
+          <Circle /><Circle /><Circle /><Circle />
+        </div>
       </div>
     </div>
   );
